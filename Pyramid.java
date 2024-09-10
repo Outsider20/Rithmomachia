33f7fc821f42d5c262f4bc8140b888736c4e52b5
Outsider20 <Spoojr848@oneonta.edu>
2024-09-10 14:13:11 -0400

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,11 +1,19 @@
 public class Pyramid extends Piece {
+
+    Circle a = a
+    Triangle b = b
+    Square c = c
+    
     public Pyramid(int value, String color) {
         super("Pyramid", value, 0, color);
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
     @Override
     public boolean isValidMove(int startX, int startY, int endX, int endY, Board board) {
-        // Implement movement logic for Pyramid
-        return true;
+        return a.isValidMove() && b.isValidMove() && c.isValidMove();
     }
 }
